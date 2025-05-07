cfddd92ffaf23641bd8ffff638cb51da67fba3c8
Greg Nazario <greg@gnazar.io>
2025-05-06 20:03:56 -0400

    update to mdbook plugins

--- install.ps1
+++
@@ -1,6 +0,0 @@
-# Note, you will need to install rust prior to running this script
-
-cargo install mdbook
-cargo install mdbook-mermaid
-cargo install mdbook-katex
-cargo install mdbook-linkcheck
--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Note, you will need to install rust prior to running this script
-
-cargo install mdbook
-cargo install mdbook-mermaid
-cargo install mdbook-katex
-cargo install mdbook-linkcheck
